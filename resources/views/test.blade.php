b8d29db32a0348df63e285d97b6cddded388a49f
AlexArtaud-Dev <smlartaudalexandre@gmail.com>
2022-12-24 23:57:57 +0100

    Favicon

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<x-guest-layout>
+    <x-darkcard>
+        <x-slot name="logo">
+            <img src="{{ asset("img/logos/logo.svg") }}" alt="Website Logo" width="10%">
+        </x-slot>
+
+        <x-slot name="slot">
+            This is the test page.
+        </x-slot>
+    </x-darkcard>
+
+</x-guest-layout>
