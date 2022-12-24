036700847b5086217b4478096bebc18f5f463f3c
AlexArtaud-Dev <smlartaudalexandre@gmail.com>
2022-12-24 18:23:16 +0100

    Favicon

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://dev1.alexartauddev.xyz'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
