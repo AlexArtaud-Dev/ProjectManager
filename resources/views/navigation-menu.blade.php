1af13ffba9e1828e4b9d76784079d4e3026b1aff
AlexArtaud-Dev <smlartaudalexandre@gmail.com>
2022-12-25 01:15:39 +0100

    Favicon

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,6 +1,6 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100 dark:bg-background-400 dark:text-primary-200">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
