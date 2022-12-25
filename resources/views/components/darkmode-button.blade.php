e33fdb380f8612d4bc5369674cba9f11837cb685
AlexArtaud-Dev <smlartaudalexandre@gmail.com>
2022-12-25 02:05:46 +0100

    Favicon

--- resources/views/components/darkmode-button.blade.php
+++ resources/views/components/darkmode-button.blade.php
@@ -1,4 +1,4 @@
-// button always displays in the bottom right corner to toggle dark mode with a sun or moon icon
+{{--// button always displays in the bottom right corner to toggle dark mode with a sun or moon icon--}}
 <button x-on:click="darkMode = !darkMode" class="fixed bottom-0 right-0 m-4 p-2 rounded-full bg-gray-100 dark:bg-background-1100 border border-gray-200 dark:border-background-1100 shadow-lg">
     <svg x-show="darkMode === false" class="w-6 h-6 text-gray-600 dark:text-background-1100" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
         <path d="M12 4v16m8-8H4"></path>
