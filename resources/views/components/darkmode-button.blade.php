935b1a2e3094f183bb73c21347651cf5b1bd9cd0
AlexArtaud-Dev <smlartaudalexandre@gmail.com>
2022-12-25 01:48:40 +0100

    Favicon

--- resources/views/components/darkmode-button.blade.php
+++ resources/views/components/darkmode-button.blade.php
@@ -1,5 +1,5 @@
 <button type="button"
-        x-bind:class="darkMode ? 'bg-primary-50' : 'bg-gray-200'" x-on:click="darkMode = !darkMode" class="relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-primary-50 focus:ring-offset-2" role="switch" aria-checked="false">
+        x-bind:class="darkMode ? 'bg-primary-300' : 'bg-gray-200'" x-on:click="darkMode = !darkMode" class="relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-primary-300 focus:ring-offset-2" role="switch" aria-checked="false">
     <span class="sr-only">Dark mode toggle</span>
     <span x-bind:class="darkMode ? 'translate-x-5 bg-secondary-200' : 'translate-x-0 bg-white'" class="pointer-events-none relative inline-block h-5 w-5 transform rounded-full shadow ring-0 transition duration-200 ease-in-out">
         <span x-bind:class="darkMode ? 'opacity-0 ease-out duration-100' : 'opacity-100 ease-in duration-200'" class="absolute inset-0 flex h-full w-full items-center justify-center transition-opacity" aria-hidden="true">
