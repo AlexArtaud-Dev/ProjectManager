3e52074e0685fddd6a137950e9b4e4b2fb636cca
AlexArtaud-Dev <smlartaudalexandre@gmail.com>
2022-12-25 01:42:54 +0100

    Favicon

--- resources/views/vendor/jetstream/components/nav-link.blade.php
+++ resources/views/vendor/jetstream/components/nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
 $classes = ($active ?? false)
             ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-primary-200 text-sm font-medium leading-5 text-gray-900 dark:text-primary-200 focus:outline-none focus:border-indigo-700 dark:focus:border-primary-400 transition'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-primary-400 hover:text-gray-700 dark:hover:text-primary-300 hover:border-gray-300 dark:border-primary-300 focus:outline-none focus:text-gray-700 dark:focus:text-primary-400 focus:border-gray-300 dark:focus:border-primary-400 transition';
+            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-primary-400 hover:text-gray-700 dark:hover:text-primary-100 hover:border-gray-300 dark:border-primary-300 focus:outline-none focus:text-gray-700 dark:focus:text-primary-400 focus:border-gray-300 dark:focus:border-primary-400 transition';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
